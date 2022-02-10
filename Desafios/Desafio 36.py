bfc53291c8b433b9e4d25ded73076e69502e32b3
pmraguiar <99078050+pmraguiar@users.noreply.github.com>
2022-02-10 11:07:37 -0300

    Resolução de 3 exercícios.

--- Desafios/Desafio 36.py
+++ Desafios/Desafio 36.py
@@ -2,6 +2,21 @@
 # o VALOR DA CASA, o SALÁRIO do comprador e em QUANTOS ANOS ele vai PAGAR. Calcule o valor da prestação mensal,
 # sabendo que ela não pode exceder 30% do salário ou então o empréstimo será negado.
 #
-print('=-=' * 19)
-print('Bem vindo ao simulador de empréstimo do Banco do Gatinho')
-print('=-=' * 19)
+from time import sleep
+print('\033[97m=-=\033[m' * 19)
+print('\033[36mBem vindo ao simulador de empréstimo do Banco do Gatinho\033[m')
+print('\033[97m=-=\033[m' * 19)
+nome = str(input('Qual o seu nome? ')).strip().title()
+casa = float(input('Qual o valor da casa que está querendo comprar? R$'))
+sal = float(input('Qual seu salário? R$'))
+tempo = int(input('Quantos anos você quer financiar? '))
+print(f'OK, {nome}, estamos analisando...')
+sleep(2)
+meses = tempo * 12
+parcela = casa / meses
+condicao = sal * 30 / 100
+if condicao >= parcela:
+    print(f'\033[35mParabéns\033[m, {nome}, \033[35mvocê conseguiu\033[m o empréstimo para a compra da sua casa. '
+          f'Você irá pagar R${parcela:.2f} durante {meses} meses.')
+else:
+    print(f'{nome}, \033[33minfelizmente\033[m, seu empréstimo foi negado.')
--- Desafios/Desafio 38.py
+++ Desafios/Desafio 38.py
@@ -3,3 +3,11 @@
 # — O segundo valo é maior;
 # — Não existe valor maior, os dois são iguais.
 #
+n1 = int(input('Diga um número: '))
+n2 = int(input('Diga outro número: '))
+if n1 > n2:
+    print(f'O {n1} é maior que {n2}.')
+elif n2  > n1:
+    print(f'O {n2} é maior que o {n1}.')
+else:
+    print('Não existe número maior, os dois são iguais.')
