482fbee0c340ec8b41b4110bf323e054d06ea798
pmraguiar <99078050+pmraguiar@users.noreply.github.com>
2022-02-11 19:01:06 -0300

    RESOLUÇAO DE EXERCICIOS

--- Desafios/Desafio 45.py
+++ Desafios/Desafio 45.py
@@ -2,16 +2,28 @@
 #
 from random import randint
 from time import sleep
+itens = ('Pedra', 'Papel', 'Tesoura')
 pc = randint(0, 2)
-print('''Suas opções:
-[ 0 ] PEDRA
+print('Vamos jogar JOKENPO??')
+sleep(1)
+print('''Escolha uma opção:
+[ 0 ] \033[37mPEDRA\033[m
 [ 1 ] PAPEL
-[ 2 ] TESOURA''')
-jogador = int(input('Qual a sua jogada?' ))
+[ 2 ] \033[35mTESOURA\033[m''')
+jogador = int(input('Qual a sua jogada? '))
 print('JO')
-sleep(1)
+sleep(0.6)
 print('KEN')
-sleep(1)
+sleep(0.6)
 print('PO!!!')
-print('-=' * 8)
-if
+sleep(0.6)
+print('-=' * 14)
+print(f'O computador jogou {itens[pc]}')
+print(f'Você jogou {itens[jogador]}')
+print('-=' * 14)
+if jogador == pc:
+    print('\033[33mEmpate!\033[m')
+elif jogador == 0 and pc == 2 or jogador == 1 and pc == 0 or jogador == 2 and pc == 1:
+    print('\033[33mVocê\033[m venceu!')
+elif jogador == 2 and pc == 0 or jogador == 0 and pc == 1 or jogador == 1 and pc == 2:
+    print('O \033[31mcomputador\033[m venceu!')
--- testes.py
+++ testes.py
@@ -1,7 +1,17 @@
-n1 = float(input('1 numero: '))
-if n1 == 1:
-    print('show.')
-elif n1 == 2:
-    print('ok.')
-elif n1 == 3:
-    print('nada mal.')
+from random import randint
+from time import sleep
+pc = randint(0, 2)
+print('''Suas opções:
+[ 0 ] PEDRA
+[ 1 ] PAPEL
+[ 2 ] TESOURA''')
+jogador = int(input('Qual a sua jogada? '))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PO!!!')
+print('-=' * 8)
+print(f'O computador jogou {pc}')
+print(f'O jogador jogou {jogador}')
+print('-=' * 8)
