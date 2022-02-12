92cde6f53015a1c3c3622deb5564e0ad46447045
pmraguiar <99078050+pmraguiar@users.noreply.github.com>
2022-02-12 09:20:32 -0300

    desafio 46

---
+++ Desafios/Desafio 46.py
@@ -0,0 +1,10 @@
+from time import sleep
+from emoji import emojize
+print(emojize(':fireworks:' * 18))
+print('VAI COMEÇAR A CONTAGEM REGRESSIVA!')
+print(emojize(':fireworks:' * 18))
+for c in range(10, -1, -1):
+    print(c)
+    sleep(1)
+print('FELIZ ANO NOVOOOO!!!')
+print(emojize('\U0001f389' * 10))
--- testes.py
+++ testes.py
@@ -1,17 +1,2 @@
-from random import randint
-from time import sleep
-pc = randint(0, 2)
-print('''Suas opções:
-[ 0 ] PEDRA
-[ 1 ] PAPEL
-[ 2 ] TESOURA''')
-jogador = int(input('Qual a sua jogada? '))
-print('JO')
-sleep(1)
-print('KEN')
-sleep(1)
-print('PO!!!')
-print('-=' * 8)
-print(f'O computador jogou {pc}')
-print(f'O jogador jogou {jogador}')
-print('-=' * 8)
+for c in range(10, -1, -1):
+    print(c)
